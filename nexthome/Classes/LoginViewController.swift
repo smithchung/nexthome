51b91cf672d14b1b6cbc5bc78595e0e9b6aaef72
Eric Seo <1994ericseo@gmail.com>
2016-03-16 01:21:44 -0700

    made launch screen, started on login screen

--- nexthome/Classes/LoginViewController.swift
+++ nexthome/Classes/LoginViewController.swift
@@ -9,11 +9,28 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let backgroundImage = UIImageView(image: UIImage(named: "loginBackground"))
+        backgroundImage.frame = self.view.frame
+        self.view.insertSubview(backgroundImage, atIndex: 0)
+        
+        let usernameIconImage = UIImageView(image: UIImage(named: "User"))
+        usernameIconImage.frame.size.width = 23
+        usernameIconImage.frame.size.height = 30
+        usernameTextField.leftViewMode = UITextFieldViewMode.Always
+        usernameTextField.leftView = usernameIconImage
+        
+        let passwordIconImage = UIImageView(image: UIImage(named: "Lock"))
+        passwordIconImage.frame.size.width = 23
+        passwordIconImage.frame.size.height = 30
+        passwordTextField.leftViewMode = UITextFieldViewMode.Always
+        passwordTextField.leftView = passwordIconImage
     }
 
     override func didReceiveMemoryWarning() {
